df163bfe8d07ca05faf3e9d4c35758ce44fdabe1
John S <johnc.sime@gmail.com>
2021-12-10 11:30:20 -0500

    Got App.js looking good, currently adding Character attributes

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
-// Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const Character = (props) => {
+    return (
+        <div>
+            <p>Name: {props.data.name}</p>
+            <p>Height: {props.data.height}</p>
+            <p>Mass: {props.data.mass}</p>
+        </div>
+    )
+}
+
+export default Character;
