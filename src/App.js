b5d428acd1041e33d6f73317ff8bb4675d5c163e
John S <johnc.sime@gmail.com>
2021-12-10 14:56:28 -0500

    got the styling done in Character.js, site should be complete now

--- src/App.js
+++ src/App.js
@@ -5,27 +5,24 @@ import axios from 'axios';
 import Character from './components/Character';
 
 const App = () => {
-  const [charCard, setCharCard] = useState([])
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [characters, setCharacters] = useState([]);
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
   useEffect(()=>{
-    axios.get('[GET] https://swapi.dev/api/people')
+    axios.get('https://swapi.dev/api/people')
     .then(res => {
-      setCharCard(res.data)
+      setCharacters(res.data);
     })
     .catch(err => {
-      console.log(err)
+      console.log('Character error');
     })
   }, [])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
-      { charCard && <Character data={charCard} /> }
+      {characters.map(char => {
+        return <Character key={char.name} data={char} />
+      })
+    }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,14 +1,42 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-const Character = (props) => {
+
+const StylePoints = styled.div`
+    color: #191970;
+    width: 35%;
+    height: 30vh;
+    /* text-align: center; */
+    display:flex;
+    flex-direction: column;
+    flex-wrap: wrap;
+    margin-left: 35%;
+    margin-bottom: 2%;
+    padding: 2%;
+    background-color: #D3D3D3;
+    border-radius: 5%;
+    border: 1px solid #000;
+
+
+    h1 {
+        font-size: 2rem;
+    }
+    h3 {
+        font-size: 1rem;
+    }
+`
+
+export default function Character(props) {
+    const { data} = props
+    
     return (
-        <div>
-            <p>Name: {props.data.name}</p>
-            <p>Height: {props.data.height}</p>
-            <p>Mass: {props.data.mass}</p>
-        </div>
+        <StylePoints>
+            <h1>{data.name}:</h1>
+            <div>
+                <h3>Birth year: {data.birth_year}</h3>
+                <h3>Sex: {data.gender}</h3>
+                <h3>Height: {data.height}</h3>
+            </div>
+        </StylePoints>
     )
 }
-
-export default Character;
